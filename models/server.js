5989b74ff158c100216f9d6a223596c4e8a37313
mivima86 <90853861+mivima86@users.noreply.github.com>
2023-08-31 19:40:00 +0200

    Update server.js - Added more options to the port

--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ class Server {
 
     constructor(){
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3050;
         this.userPath = '/api/users';
 
 
